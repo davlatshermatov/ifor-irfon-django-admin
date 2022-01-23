f66f26f10f6e0b6882e43fcdeedbbe32cfb11996
Davlat Shermatov <davlatshermatov111@gmail.com>
2022-01-23 19:43:18 +0500

    updated urls.py

--- eshop/urls.py
+++ eshop/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('', admin.site.urls),
-    path('api/', include('api.urls'))
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
